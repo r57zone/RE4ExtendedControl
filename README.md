157b7e14ce41b1c0126b530babfa30b63395bae2
r57zone <r57zone@gmail.com>
2023-10-13 23:34:22 +0400

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# RE4ExtendedControl
-Extended control for Resident Evil 4 (2014) / Расширенное управление для Resident Evil 4 (2014)
+[![EN](https://user-images.githubusercontent.com/9499881/33184537-7be87e86-d096-11e7-89bb-f3286f752bc6.png)](https://github.com/r57zone/RE4ExtendedControl/) 
+[![RU](https://user-images.githubusercontent.com/9499881/27683795-5b0fbac6-5cd8-11e7-929c-057833e01fb1.png)](https://github.com/r57zone/RE4ExtendedControl/blob/master/README.RU.md)
+← Choose language | Выберите язык
+
+# Resident Evil 4 Extended Control
+Resident Evil 4 (2014) extended control for Xbox gamepads. The mod adds support for quick weapon switching, changes the run, inventory, and Ashley command buttons, and adds aiming to the right stick.
+
+# Control
+[![](https://github.com/r57zone/RE4ExtendedControl/assets/9499881/cfe5f18c-53bd-492c-b6f0-60f68fbc2d7f)](https://youtu.be/)
+
+The inventory is moved to the `Y` button and the map to the `Start` button. The start button is used to move items in the inventory.
+
+
+The run button is duplicated on the `RB` button.
+
+
+The DPAD buttons switch weapons through emulation of the inventory and action buttons. Weapons should be laid out in inventory from left to bottom, the first 4 weapons will switch to DPAD, the 4th weapon should be used for the sniper rifle. Selecting the 4th weapon is different because it turns on the aiming mode which swaps the stick places, if the sniper rifle is selected through the menu, you can turn on the mode by pressing the right stick. If after selecting the sniper rifle something else is selected through the inventory, then click again on the right stick the mode will be reset.
+
+## Setup
+1. Extract [archive](https://github.com/r57zone/RE4ExtendedControl/releases) to the game folder `C:\Games\ResidentEvil4\Bin32` (in the subfolder `Bin32`).
+2. If necessary, change the parameters in the `xinput1_3.ini` configuration file. You can disable right stick aiming and quick access to weapons by default, also you can disable them during the game on the keys `ALT + 9` and `ALT + 0`.
+3. In the game, go to Settings → Joystick Settings → Xbox 360 Joystick → Button Binding → Type II.
+
+## Download
+>Version for Windows 7, 8.1, 10, 11.
+
+**[Download](https://github.com/r57zone/RE4ExtendedControl/releases)**
+
+## Feedback
+`r57zone[at]gmail.com`
---
+++ Source/XInput.cpp
@@ -0,0 +1,228 @@
+#include <windows.h>
+#include "XInput.h"
+#include "IniReader\IniReader.h"
+#pragma comment(lib, "XInput.lib")
+
+#define DLLEXPORT extern "C" __declspec(dllexport)
+
+int TickCounter = 0;
+bool FastAccessLeft = false;
+bool FastAccessUp = false;
+bool FastAccessRight = false;
+bool FastAccessDown = false;
+bool SniperRiffleMode = false;
+
+int SkipPollCount = 0;
+bool FastAccess = true;
+bool RightStickAIM = true;
+
+DLLEXPORT BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		CIniReader IniFile("xinput1_3.ini");
+		FastAccess = IniFile.ReadBoolean("Main", "FastAccess", true);
+		RightStickAIM = IniFile.ReadBoolean("Main", "RightStickAIM", true);
+		break;
+	}
+	return TRUE;
+}
+
+DLLEXPORT DWORD WINAPI XInputGetState2(_In_ DWORD dwUserIndex, _Out_ XINPUT_STATE *pState)
+{
+	int Res = XInputGetState(dwUserIndex, pState);
+
+	if (SkipPollCount == 0 && (GetAsyncKeyState(VK_LMENU) & 0x8000) != 0 && (GetAsyncKeyState('9') & 0x8000) != 0) {
+		FastAccess = !FastAccess;
+		SkipPollCount = 30;
+	}
+
+	if (SkipPollCount == 0 && (GetAsyncKeyState(VK_LMENU) & 0x8000) != 0 && (GetAsyncKeyState('0') & 0x8000) != 0) {
+		RightStickAIM = !RightStickAIM;
+		SkipPollCount = 30;
+	}
+
+	if (Res == ERROR_SUCCESS) {
+		if (pState->Gamepad.wButtons & XINPUT_GAMEPAD_START) {
+			pState->Gamepad.wButtons ^= XINPUT_GAMEPAD_START;
+			pState->Gamepad.wButtons |= XINPUT_GAMEPAD_Y;
+		}
+		else if (pState->Gamepad.wButtons & XINPUT_GAMEPAD_Y) {
+			pState->Gamepad.wButtons ^= XINPUT_GAMEPAD_Y;
+			pState->Gamepad.wButtons |= XINPUT_GAMEPAD_START;
+		}
+
+		if (pState->Gamepad.bLeftTrigger > 0 && pState->Gamepad.wButtons & XINPUT_GAMEPAD_X)
+			pState->Gamepad.wButtons |= XINPUT_GAMEPAD_A;
+
+		if (RightStickAIM && pState->Gamepad.bLeftTrigger > 0) {
+			if (SniperRiffleMode == false) {
+				if (abs(pState->Gamepad.sThumbRX) > 0)
+					pState->Gamepad.sThumbLX = pState->Gamepad.sThumbRX;
+				if (abs(pState->Gamepad.sThumbRY) > 0)
+					pState->Gamepad.sThumbLY = pState->Gamepad.sThumbRY;
+			} else { 
+				SHORT TempAxis = 0;
+				TempAxis = pState->Gamepad.sThumbLX;
+				pState->Gamepad.sThumbLX = pState->Gamepad.sThumbRX;
+				pState->Gamepad.sThumbRX = TempAxis;
+				TempAxis = pState->Gamepad.sThumbLY;
+				pState->Gamepad.sThumbLY = pState->Gamepad.sThumbRY;
+				pState->Gamepad.sThumbRY = TempAxis;
+			}
+
+		}
+
+		if (pState->Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) {
+			pState->Gamepad.wButtons ^= XINPUT_GAMEPAD_RIGHT_SHOULDER;
+			pState->Gamepad.wButtons |= XINPUT_GAMEPAD_A;
+		}
+
+		if (pState->Gamepad.wButtons & XINPUT_GAMEPAD_B)
+			pState->Gamepad.wButtons |= XINPUT_GAMEPAD_RIGHT_SHOULDER;
+
+		if (SkipPollCount == 0 && pState->Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_THUMB) {
+			SniperRiffleMode = !SniperRiffleMode;
+			SkipPollCount = 30;
+		}
+
+		if (FastAccess) {
+			if (pState->Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT) {
+				pState->Gamepad.wButtons ^= XINPUT_GAMEPAD_DPAD_LEFT;
+				if (TickCounter == 0 && FastAccessUp == false && FastAccessLeft == false && FastAccessRight == false && FastAccessDown == false) {
+					FastAccessLeft = true;
+					SniperRiffleMode = false;
+					TickCounter = 230;
+				}
+			}
+			else if (pState->Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP) {
+				pState->Gamepad.wButtons ^= XINPUT_GAMEPAD_DPAD_UP;
+				if (TickCounter == 0 && FastAccessUp == false && FastAccessLeft == false && FastAccessRight == false && FastAccessDown == false) {
+					FastAccessUp = true;
+					SniperRiffleMode = false;
+					TickCounter = 255;
+				}
+			}
+			else if (pState->Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT) {
+				pState->Gamepad.wButtons ^= XINPUT_GAMEPAD_DPAD_RIGHT;
+				if (TickCounter == 0 && FastAccessUp == false && FastAccessLeft == false && FastAccessRight == false && FastAccessDown == false) {
+					FastAccessRight = true;
+					SniperRiffleMode = false;
+					TickCounter = 285;
+				}
+			}
+			else if (pState->Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN) {
+				pState->Gamepad.wButtons ^= XINPUT_GAMEPAD_DPAD_DOWN;
+				if (TickCounter == 0 && FastAccessUp == false && FastAccessLeft == false && FastAccessRight == false && FastAccessDown == false) {
+					FastAccessDown = true;
+					SniperRiffleMode = true;
+					TickCounter = 315;
+				}
+			}
+		}
+
+		if (FastAccessLeft) {
+			if (TickCounter < 230 && TickCounter > 180)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_START;
+		}
+
+		if (FastAccessUp) {
+			if (TickCounter < 255 && TickCounter > 205)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_START;
+			else if (TickCounter < 205 && TickCounter > 180)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_DPAD_DOWN;
+		}
+
+		if (FastAccessRight) {
+			if (TickCounter < 285 && TickCounter > 235)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_START;
+			else if (TickCounter < 235 && TickCounter > 180)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_DPAD_DOWN;
+		}
+
+		if (FastAccessDown) {
+			if (TickCounter < 315 && TickCounter > 265)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_START;
+			else if (TickCounter < 265 && TickCounter > 180)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_DPAD_DOWN;
+		}
+
+		if (FastAccessLeft || FastAccessUp || FastAccessRight || FastAccessDown) {
+			if (TickCounter < 180 && TickCounter > 130)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_A;
+			else if (TickCounter < 100 && TickCounter > 50)
+				pState->Gamepad.wButtons |= pState->Gamepad.wButtons |= XINPUT_GAMEPAD_A;
+			else if (TickCounter < 50 && TickCounter > 1)
+				pState->Gamepad.wButtons |= XINPUT_GAMEPAD_START;
+		}
+
+		if (TickCounter > 0) {
+			if (TickCounter == 1) {
+				FastAccessLeft = false;
+				FastAccessUp = false;
+				FastAccessRight = false;
+				FastAccessDown = false;
+			}
+			TickCounter--;
+		}
+		if (SkipPollCount > 0)
+			SkipPollCount--;
+	}
+
+	return Res;
+}
+
+DLLEXPORT DWORD WINAPI XInputSetState2(_In_ DWORD dwUserIndex, _In_ XINPUT_VIBRATION *pVibration)
+{
+	return XInputSetState(dwUserIndex, pVibration);
+}
+
+
+DLLEXPORT DWORD WINAPI XInputGetCapabilities2(_In_ DWORD dwUserIndex, _In_ DWORD dwFlags, _Out_ XINPUT_CAPABILITIES *pCapabilities)
+{
+	return ERROR_SUCCESS;
+}
+
+DLLEXPORT void WINAPI XInputEnable2(_In_ BOOL enable)
+{
+
+}
+
+DLLEXPORT DWORD WINAPI XInputGetDSoundAudioDeviceGuids2(DWORD dwUserIndex, GUID* pDSoundRenderGuid, GUID* pDSoundCaptureGuid)
+{
+	return ERROR_SUCCESS;
+}
+
+DLLEXPORT DWORD WINAPI XInputGetBatteryInformation2(_In_ DWORD dwUserIndex, _In_ BYTE devType, _Out_ XINPUT_BATTERY_INFORMATION *pBatteryInformation)
+{
+	return ERROR_SUCCESS;
+}
+
+DLLEXPORT DWORD WINAPI XInputGetKeystroke2(DWORD dwUserIndex, DWORD dwReserved, PXINPUT_KEYSTROKE pKeystroke)
+{
+	return ERROR_SUCCESS;
+}
+
+DLLEXPORT DWORD WINAPI XInputGetStateEx2(_In_ DWORD dwUserIndex, _Out_ XINPUT_STATE *pState)
+{
+	return ERROR_SUCCESS;
+}
+
+DLLEXPORT DWORD WINAPI XInputWaitForGuideButton2(_In_ DWORD dwUserIndex, _In_ DWORD dwFlag, _In_ LPVOID pVoid)
+{
+	return ERROR_SUCCESS;
+}
+
+DLLEXPORT DWORD XInputCancelGuideButtonWait2(_In_ DWORD dwUserIndex)
+{
+	return ERROR_SUCCESS;
+}
+
+DLLEXPORT DWORD XInputPowerOffController2(_In_ DWORD dwUserIndex)
+{
+	return ERROR_SUCCESS;
+}
